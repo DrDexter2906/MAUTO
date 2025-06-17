45aa05175f1130e1e0a6524ca9014f3eb710692f
jacopobigoni898 <jacopo.bigoni@edu-its.it>
2025-06-17 20:42:38 +0200

    1 Giorno di lavoro 17/06/2025

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mauto"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mauto"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -36,7 +36,10 @@ android {
 }
 
 dependencies {
-
+    //inserito per usare le card per utilizzar i radius
+    implementation ("com.google.android.material:material:1.x.x") // Usa l'ultima versione disponibile
+    //serve per la navigation bar
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/mauto/MainActivity.kt
+++ app/src/main/java/com/example/mauto/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //recupero l id della navigation bar
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val HomeFragment = HomeFragment()
+        val CollectionFragment = CollectionFragment()
+
+        //setto il fragment iniziale
+        setCurrentFragment(HomeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(HomeFragment)
+                R.id.collection -> setCurrentFragment(CollectionFragment)
+            }
+            true
+        }
+
+    }
+    //serve a settare com eprimo fragment in questo caso la home
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
         }
     }
-}
